622b9e38656344616e72f4ea1502a438717e01fa
xamarin <xamarin@Halimas-MacBook-Air.local>
2022-05-24 13:20:32 -0700

    Update

--- pages/api/solpay.ts
+++ pages/api/solpay.ts
@@ -1,14 +1,18 @@
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
 import { clusterApiUrl, Connection, PublicKey, SystemProgram, LAMPORTS_PER_SOL } from "@solana/web3.js"
 import { NextApiRequest, NextApiResponse } from "next"
-import { Keypair, Transaction } from "@solana/web3.js";
+import { createQR, encodeURL, TransferRequestURLFields, findReference, validateTransfer, FindReferenceError, ValidateTransferError } from "@solana/pay";
+import internal from "stream";
+import BigNumber from "bignumber.js";
 
 export type MakeTransactionInputData = {
-  account: string,
+  reference: string,
+  amount: string,
+  shopAddress: string,
 }
 
 export type MakeTransactionOutputData = {
-  transaction: string,
+  signature: string,
   message: string,
 }
 
@@ -20,79 +24,47 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<MakeTransactionOutputData | ErrorOutput>
 ) {
-  const { reference } = req.query
   try {
-    // We pass the selected items in the query, calculate the expected cost
-    const amount = parseInt("100")
-    if (amount === 0) {
-      res.status(400).json({ error: "Can't checkout with charge of 0" })
-      return
-    }
-
-    // We pass the reference to use in the query
-    if (!reference) {
-      res.status(400).json({ error: "No reference provided" })
-      return
-    }
-     // We pass the buyer's public key in JSON body
-     const { account } = req.body as MakeTransactionInputData
-     if (!account) {
-       res.status(400).json({ error: "No account provided" })
-       return
-     }
-
-    const buyerPublicKey = new PublicKey(account)
-    const shopPublicKey = new PublicKey("3sCcDnsfpwe9YeFvcwhqdGDZUoibYsT76UwcGfRKZtnB")
-
+    const input_data  = req.query as MakeTransactionInputData;
+    const  amount = input_data.amount;
+    const shopAddress = input_data.shopAddress
+    const reference = input_data.reference
     const network = WalletAdapterNetwork.Devnet
-    const endpoint = clusterApiUrl(network)
-    const connection = new Connection(endpoint)
-
-    // Get a recent blockhash to include in the transaction
-    const { blockhash } = await (connection.getLatestBlockhash('finalized'))
-
-    const transaction = new Transaction({
-      recentBlockhash: blockhash,
-      // The buyer pays the transaction fee
-      feePayer: buyerPublicKey,
-    })
-
-    // Create the instruction to send SOL from the buyer to the shop
-    const transferInstruction = SystemProgram.transfer({
-      fromPubkey: buyerPublicKey,
-      lamports: amount * (LAMPORTS_PER_SOL),
-      toPubkey: shopPublicKey,
-    })
-
-    // Add the reference to the instruction as a key
-    // This will mean this transaction is returned when we query for the reference
-    transferInstruction.keys.push({
-      pubkey: new PublicKey(reference),
-      isSigner: false,
-      isWritable: false,
-    })
-
-    // Add the instruction to the transaction
-    transaction.add(transferInstruction)
-
-    // Serialize the transaction and convert to base64 to return it
-    const serializedTransaction = transaction.serialize({
-      // We will need the buyer to sign this transaction after it's returned to them
-      requireAllSignatures: false
-    })
-    const base64 = serializedTransaction.toString('base64')
-
-    // Insert into database: reference, amount
-
-    // Return the serialized transaction
-    res.status(200).json({
-      transaction: base64,
-      message: "Thanks for your order! 🍪",
-    })
-  } catch (err) {
-    console.error(err);
-
-    res.status(500).json({ error: 'error creating transaction', })
-    return
-  }
+const endpoint = clusterApiUrl(network)
+const connection = new Connection(endpoint);
+const urlParams: TransferRequestURLFields = {
+  recipient: new PublicKey(shopAddress),
+  splToken: new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr"),
+  amount: new BigNumber(Number(amount)),
+  reference: new PublicKey(reference),
+  label: "Cookies Inc",
+  message: "Thanks for your order! 🍪",
 }
+    // Check if there is any transaction for the reference
+    const signatureInfo = await findReference(connection, new PublicKey(reference), { finality: 'confirmed' })
+    // Validate that the transaction has the expected recipient, amount and SPL token
+    const data = await validateTransfer(
+      connection,
+      signatureInfo.signature,
+      {
+        recipient: new PublicKey(shopAddress),
+        amount: new BigNumber(Number(amount)),
+        splToken: new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr"),
+        reference: new PublicKey(reference),
+      },
+      { commitment: 'confirmed' }
+    )
+
+            console.log(data);
+              res.status(200).json({
+                    signature: "recieved",
+                    message:  "Thanks for your order! 🍪",
+                  });}
+                   catch
+                   {
+                     res.status(200).json({
+                        signature:"not_yet",
+                        message:  "be patient my guy",
+                  });
+    }
+  }
